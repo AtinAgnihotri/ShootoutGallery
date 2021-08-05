5e5849cf96b76e351d829819f8c2690ac2af69d4
AtinAgnihotri <atinagnihotri@gmail.com>
2021-08-05 21:55:03 +0530

    Adds background

--- ShootingGallery/GameScene.swift
+++ ShootingGallery/GameScene.swift
@@ -10,9 +10,57 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    override func didMove(to view: SKView) {
-        
+    var waterPlate: SKSpriteNode!
+    var waterInPeak = false
+    var waterTexture: String {
+        waterInPeak ? "water-bg" : "water-fg"
     }
+    
+    override func didMove(to view: SKView) {
+        setupBackground()
+    }
+    
+    func setupBackground() {
+        backgroundColor = .clear
+        addWoodPlate()
+        addForrestPlate()
+        addCurtainPlate()
+    }
+    
+    func addWoodPlate() {
+        let wood = SKSpriteNode(imageNamed: "wood-background")
+        wood.blendMode = .replace
+        wood.position = CGPoint(x: 512, y: 384)
+        wood.xScale = 1.5
+        wood.yScale = 1.5
+        wood.zPosition = -3
+        addChild(wood)
+    }
+    
+    func addForrestPlate() {
+        let forrest = SKSpriteNode(imageNamed: "grass-trees")
+        forrest.position = CGPoint(x: 512, y: 384)
+        forrest.xScale = 1.5
+        forrest.yScale = 1.5
+        forrest.zPosition = -2
+        addChild(forrest)
+    }
+    
+    func addCurtainPlate() {
+        let curtain = SKSpriteNode(imageNamed: "curtains")
+        curtain.position = CGPoint(x: 512, y: 384)
+        curtain.xScale = 1.5
+        curtain.yScale = 1.5
+        curtain.zPosition = 2
+        curtain.name = "curtain"
+        addChild(curtain)
+    }
+    
+    /*
+    func addWaterPlate() {
+        waterPlate =
+    }
+    */
     
     
     override func update(_ currentTime: TimeInterval) {
